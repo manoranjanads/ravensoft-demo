d37b2ad7a8c9a9d0d0e84d42e8b4242983823331
ManoRanjana De Silva <manoranjanad@embla.asia>
2020-03-09 18:27:57 +0530

    init commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,29 @@
-import React from 'react'
-import config from '../../../config'
+import React from "react";
+import config from "../../../config";
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <div className='container'>
-        <div className='content has-text-centered'>
+    <footer className="footer">
+      <div className="container">
+        <div className="content has-text-centered">
+          <p>{config.copyright}</p>
           <p>
-            {config.copyright}
+            Developed by <a href="mailto://manoranjana@ravensoft.tech">MR</a> in{" "}
+            <a href="https://www.ravensoft.tech" target="_blank">
+              Ravensoft
+            </a>{" "}
+            |{" "}
+            <a
+              href="https://github.com/MRGitHub94/ravensoft-demo"
+              target="_blank"
+            >
+              Github Repository
+            </a>
           </p>
-          <p>Powered by <a href='https://www.gatsbyjs.org'>Gatsby</a> and <a href='https://www.netlifycms.org'>Netlify CMS</a> | <a href='https://github.com/v4iv/gatsby-starter-business'>Github Repository</a></p>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
